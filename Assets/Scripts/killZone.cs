0f66cc354caf1ef67a501b2fdd41fa93d2ca8781
Roberto Cordova <rob_cord@aol.com>
2020-04-03 20:13:02 -0700

    committed
    copied files and folders from game

---
+++ Assets/Scripts/killZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killZone : MonoBehaviour
+{
+    //Should player fall on kill zone a die function is called 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            collision.GetComponent<Platformer>().Die();
+        }
+    }
+}
